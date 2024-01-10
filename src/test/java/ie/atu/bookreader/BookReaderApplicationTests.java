3bf37999916541314dbaa239031432c167a799d9
Iwo <g00391231@atu.ie>
2024-01-10 03:06:12 +0000

    SETUP

---
+++ src/test/java/ie/atu/bookreader/BookReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.bookreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
